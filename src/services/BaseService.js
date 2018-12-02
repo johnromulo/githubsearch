5be93249a14155b75f625d2b364bfd901f56e9d3
John Rômulo <johnromulo0801987@gmail.com>
2018-12-02 20:18:14 -0300

    - service clean

--- src/services/BaseService.js
+++ src/services/BaseService.js
@@ -1,23 +1,10 @@
 import Axios from 'axios';
 import env from '../config';
 
-
 export class BaseService {
-
-  static axeioConfig() {
-    return Axios.create({
-      baseURL: env.BASE_URL,
-      crossDomain: true,
-    });
-  }
-
-  static post() {
-
-  }
 
   static get(endpoint, params) {
     return new Promise((resolve, reject) => {
-      //   const axios = this.axeioConfig();
       Axios.get(`${env.BASE_URL}${endpoint}`, {
         crossDomain: true,
         params: params
@@ -31,11 +18,4 @@ export class BaseService {
     });
   }
 
-  static put() {
-
-  }
-
-  static delete() {
-
-  }
 }
