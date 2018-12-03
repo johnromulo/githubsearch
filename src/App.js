d5d6be9a27273388f9ce041405bc2431f66c3959
TRYFV03\Owner <john.romulo@trinitysolucoes.com>
2018-12-03 10:31:22 -0200

    - router not found

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { BrowserRouter, Route, Switch, PropsRoute } from 'react-router-dom';
 import Home from './components/home/';
 import Result from './components/result/';
+import NotFound from './components/notfound';
 import store from './store';
 
 
@@ -14,6 +15,7 @@ class App extends Component {
           <Switch>
             <Route exact path='/' component={Home} />
             <Route exact path='/:filter' component={Result} />
+            <Route render={(props) => <NotFound {...props} text="Page not found!" />} />
           </Switch>
         </BrowserRouter>
       </Provider>
